50afc4b5b3011ed1f8ac68a677dcd15f555f2c08
Raysel <141379819+Rayfel2@users.noreply.github.com>
2023-10-03 18:43:30 -0400

    Filtros corregidos y mutation casi terminada

--- api3/GraphQL/InventoryTypes.cs
+++ api3/GraphQL/InventoryTypes.cs
@@ -1,6 +1,95 @@
-﻿namespace api3.GraphQL
+﻿using api3.Models;
+using HotChocolate;
+using HotChocolate.Data;
+
+namespace api3.GraphQL
 {
-    public class InventoryTypes
+    
+    public class InventoryInput
     {
+        public ShopInput Shop { get; set; }
+        public EmployeeInput Employee { get; set; }
+        public IcecreamInput Icecream { get; set; }
+        public string Date { get; set; }
     }
+
+    public class IcecreamInput
+    {
+        public string Flavor { get; set; }
+        public int Count { get; set; }
+        public bool IsSeasonFlavor { get; set; }
+    }
+
+    public class ShopInput
+    {
+        public string Name { get; set; }
+    }
+
+    public class EmployeeInput
+    {
+        public string Name { get; set; }
+    }
+    
+
+    public class InventoryType : ObjectType<Inventory>
+{
+    protected override void Configure(IObjectTypeDescriptor<Inventory> descriptor)
+    {
+        descriptor.Field(i => i.IdInventory).Type<IdType>();
+        descriptor.Field(i => i.IdStore).Type<IdType>();
+        descriptor.Field(i => i.IdEmployee).Type<IdType>();
+        descriptor.Field(i => i.Date).Type<DateTimeType>();
+        descriptor.Field(i => i.Flavor).Type<StringType>();
+        descriptor.Field(i => i.IsSeasonFlavor).Type<BooleanType>();
+        descriptor.Field(i => i.Quantity).Type<IntType>();
+    }
+}
+
+
+
+public class Query
+{
+    [UseProjection]
+    [HotChocolate.Data.UseFiltering]
+    [HotChocolate.Data.UseSorting]
+    public IQueryable<Inventory> GetInventory([Service] PgAdminContext dbContext) =>
+        dbContext.Inventories;
+    
+    public IQueryable<Inventory> InventoryByFlavor([Service] PgAdminContext dbContext, string flavor) =>
+        dbContext.Inventories.Where(i => i.Flavor == flavor);
+
+    public IQueryable<Inventory> InventoryByShop([Service] PgAdminContext dbContext, string shop) =>
+        dbContext.Inventories.Where(i => i.IdStoreNavigation.Name == shop);
+
+        public IQueryable<Inventory> InventoryByDate([Service] PgAdminContext dbContext, string date)
+        {
+            if (DateTime.TryParse(date, out DateTime parsedDate))
+            {
+                // Asegurarse de que la fecha sea en formato UTC
+                parsedDate = DateTime.SpecifyKind(parsedDate, DateTimeKind.Utc);
+
+                return dbContext.Inventories.Where(i => i.Date == parsedDate.Date);
+            }
+            else
+            {
+                // Manejo de error si la conversión de fecha falla
+                return Enumerable.Empty<Inventory>().AsQueryable();
+            }
+        }
+
+
+
+    }
+    /*
+    public class Mutation
+    {
+       public Inventory AddInventory([Service] PgAdminContext dbContext, InventoryInput input)
+        {
+            // Implementa lógica para agregar un registro de inventario a la base de datos
+            // Utiliza el parámetro de entrada para crear un objeto Inventory y guardarlo en la base de datos
+        }
+    }*/
+
+
+
 }
--- api3/Program.cs
+++ api3/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
 using api3.Models;
-using System.Runtime.Serialization;
-using Microsoft.Extensions.Options;
+using api3.GraphQL;
 using api3.Interface;
 using api3.Repository;
+using HotChocolate;
+using HotChocolate.Types;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,31 +16,40 @@ builder.Services.AddScoped<InterfaceEmployee, RepositoryEmployee>();
 builder.Services.AddScoped<InterfaceStore, RepositoryStore>();
 builder.Services.AddScoped<InterfaceInventory, RepositoryInventory>();
 builder.Services.AddMemoryCache();
-// Add services to the container.
 
 builder.Services.AddDbContext<PgAdminContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-
-
 });
 
+builder.Services.AddGraphQLServer()
+    .AddQueryType<Query>()
+    .AddProjections()
+    .AddSorting()
+    .AddFiltering()
+   .AddType<InventoryType>()
+    //  .AddMutationType<Mutation>()
+    //  .AddType<ShopType>()
+    //.AddType<EmployeeType>()
+    ;
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapGraphQL();
+
 
 app.Run();
+
